323565c533f8990d5061078db767d10c4e9625a8
Franco Tejero <40438410+francomaxtc@users.noreply.github.com>
2023-04-13 04:37:16 -0500

    Update streamlit_app.py
    added header and text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('Buenas buenas')
+streamlit.header('Esta es una prueba')
+streamlit.text('No se ni para que sirve esto')
+streamlit.text('Pero ta bonito')
+streamlit.text('Pa mi circo')
