4c36d3e9f739728c7a864978cb574841e9cc2771
Franco Tejero <40438410+francomaxtc@users.noreply.github.com>
2023-04-20 10:03:28 +0200

    Update streamlit_app.py
    alaraking table

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,4 +23,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+# Alarakes the json line into table 
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Shows table alaraked
+streamlit.dataframe(fruityvice_normalized)
